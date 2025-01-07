a664dbc252c1d5354d80a47105a01cf847453ad8
steph-leo <leostephs89@gmail.com>
2025-01-07 20:09:50 +0300

    collage student data

---
+++ collage_student_data/collage.java
@@ -0,0 +1,16 @@
+public class collage {
+    public static void main (String[] args){
+        String collageName = "COICT";
+        int studentID = 2023-02-00411;
+        String StudentName = "Stephano Siame";
+        String courseName = "Diploma in Computer Science";
+        int yearOfStudy = 2;
+
+        System.out.println("Collage Name: " + collageName);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student Name: " + StudentName);
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Year of Study: " + yearOfStudy);
+
+    }
+}
