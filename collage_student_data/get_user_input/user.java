e0355aedf61cc44cd9093685af6d806a4c3c3c0d
steph-leo <leostephs89@gmail.com>
2025-01-14 12:51:06 +0300

    initial file

--- collage_student_data/get_user_input/user.java
+++ collage_student_data/get_user_input/user.java
@@ -1,3 +1,10 @@
-public class user {
-    
+import java.util.Scanner;
+class user{
+    public static void main(String[] args){
+        Scanner name = new Scanner(System.in);
+        String user_name;
+        System.out.println("user name");
+        user_name = name.nextLine();
+        System.out.println(user_name);
+    }
 }
