14003b093b406f29e8af3df380ca7e38d6765876
Stephano Siame <141557641+steph-leo@users.noreply.github.com>
2025-01-14 12:53:23 +0300

    Update user.java

--- collage_student_data/get_user_input/user.java
+++ collage_student_data/get_user_input/user.java
@@ -3,7 +3,7 @@ class user{
     public static void main(String[] args){
         Scanner name = new Scanner(System.in);
         String user_name;
-        System.out.println("user name");
+        System.out.println("user name ");
         user_name = name.nextLine();
         System.out.println(user_name);
     }
